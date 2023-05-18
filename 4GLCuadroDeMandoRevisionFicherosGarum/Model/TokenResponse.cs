6affee0116df55cd344d6c7818264cf2f8880eff
José Antonio Lax <jalax@4glsp.com>
2023-05-18 18:30:45 +0200

    Creacion de formulario

---
+++ 4GLCuadroDeMandoRevisionFicherosGarum/Model/TokenResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 4GLCuadroDeMandoGarum.Model
+{
+    public class TokenResponse
+    {
+
+        public string Token { get; set; }
+
+        public DateTime Expiration { get; set; }
+
+        public DateTime ExpirationLocal => Expiration.ToLocalTime();
+    }
+}
